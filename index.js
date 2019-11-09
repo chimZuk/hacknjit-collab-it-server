ccedc26c4d02ce9f04324c70f27550eeb1ed6e07
Dzmitry Kuzmitch <kuzya19989@outlook.com>
2019-11-09 18:44:56 -0500

    Changed header

--- index.js
+++ index.js
@@ -16,11 +16,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../collab-it/')));
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers',
-        'X-Requested-With,content-type,x-access-token');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token');
     res.setHeader('Access-Control-Expose-Headers', 'x-access-token');
 });
 app.get('*', (req, res) => {
