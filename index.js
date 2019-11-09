61f6b0e0803a1d0fbd52f5dd81e0e87639928f11
Dzmitry Kuzmitch <kuzya19989@outlook.com>
2019-11-09 14:59:29 -0500

    Updated package script

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 80
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Get our API routes
+const api = require('./routes/api');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist/chimZuk')));
+
+// Set our api routesclea
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../collab-it/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
 
 /*const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://chimzuk:4115Mama@collab-iw4gh.azure.mongodb.net/test?retryWrites=true&w=majority";
