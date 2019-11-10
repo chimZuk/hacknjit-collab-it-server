f77f0de4c08cb02940046984a34e42e2f6d59b7f
Dzmitry Kuzmitch <kuzya19989@outlook.com>
2019-11-09 23:41:02 -0500

    Added mew socket requests

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ let socketIO = require('socket.io');
 let io = socketIO(server);
 let api = require('./routes/api');
 
-const port = process.env.PORT || 80;
+let port = process.env.PORT || 80;
+
+var messageCounter = 0;
 
 app.use('/api', api);
 app.use(bodyParser.json());
@@ -25,15 +27,115 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../collab-it/index.html'));
 });
 
+let word = "";
+
+let players = [];
+
+let messages = [];
+
 io.on('connection', (socket) => {
-    console.log('user connected');
+    console.log('CONNECTION: User connected');
+
+    io.emit('users', players.sort((a, b) => (a.score < b.score) ? 1 : -1));
+    io.emit('messages', messages);
+
+    socket.on('user-joined', (user) => {
+        var pushed = false;
+        user.main = false;
+        user.score = 0;
+        for (var i = 0; i < players.length; i++) {
+            if (players[i].UserName == user.UserName) {
+                io.emit('user-joined', {
+                    userData: players[i],
+                    players: players.sort((a, b) => (a.score < b.score) ? 1 : -1)
+                });
+                pushed = true;
+                break;
+            }
+        }
+        if (!pushed) {
+            players.push(user);
+            io.emit('user-joined', { userData: user, players: players.sort((a, b) => (a.score < b.score) ? 1 : -1) });
+        }
+    });
 
     socket.on('new-message', (message) => {
-        console.log(message);
+        message.messageID = messageCounter;
+        message.hotness = 0;
+
         io.emit('new-message', message);
-        console.log("here")
+        messageCounter++;
+
+        messages.push(message);
+
+        if (message.text.trim().toLowerCase() == word) {
+            clearInterval(timeInterval);
+            clearTimeout(gameTimeout);
+            restartGame(true, message);
+        }
+    });
+
+    socket.on('message-hotness', (message) => {
+        for (var i = 0; i < messages.length; i++) {
+            if (message.messageID == messages[i].messageID) {
+                messages[i] = message;
+            }
+        }
+        io.emit('message-hotness', message);
     });
 });
+
+let timeInterval;
+let gameTimeout;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function restartGame(hasWinner, message = {}) {
+    var secsCounter = 60;
+    word = "pizza";
+    messages = [];
+
+    for (var i = 0; i < players.length; i++) {
+        players[i].main = false;
+        if (players[i].UserName == message.senderName) {
+            players[i].score++;
+            players[i].main = true;
+            io.emit('endgame', {
+                nextPlayer: players[i],
+                word: word,
+                hasWinner: hasWinner
+            });
+        }
+    }
+
+    if (!hasWinner && players.length != 0) {
+        var nextPlayer = getRandomInt(players.length);
+        players[nextPlayer].main = true;
+        io.emit('endgame', {
+            nextPlayer: players[nextPlayer],
+            word: word,
+            hasWinner: hasWinner
+        });
+    }
+
+    io.emit('users', players.sort((a, b) => (a.score < b.score) ? 1 : -1));
+    io.emit('messages', messages);
+
+    timeInterval = setInterval(function() {
+        console.log(secsCounter + "s");
+        io.emit('time', secsCounter);
+        secsCounter--;
+    }.bind(this), 1000);
+
+    gameTimeout = setTimeout(function() {
+        clearInterval(timeInterval);
+        restartGame(false);
+    }.bind(this), 61000)
+}
+
+restartGame();
 
 server.listen(port, () => {
     console.log(`started on port: ${port}`);
